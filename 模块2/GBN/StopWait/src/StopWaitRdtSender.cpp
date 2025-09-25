f82bd481ddb9db3e8efb8ed9116578ec8c4ce703
dlzwtk <2020694283@qq.com>
2025-09-25 16:19:57 +0800

    shit

--- 模块2/GBN/StopWait/src/StopWaitRdtSender.cpp
+++ 模块2/GBN/StopWait/src/StopWaitRdtSender.cpp
@@ -2,70 +2,82 @@
 #include "Global.h"
 #include "StopWaitRdtSender.h"
 
-
-StopWaitRdtSender::StopWaitRdtSender():expectSequenceNumberSend(0),waitingState(false)
+GBNSender::GBNSender() : base(1), nextSeqNum(1), expectSequenceNumberSend(0), waitingState(false), numberOfPacketsInWindow(0)
 {
 }
 
-
-StopWaitRdtSender::~StopWaitRdtSender()
+GBNSender::~GBNSender()
 {
 }
 
-
-
-bool StopWaitRdtSender::getWaitingState() {
+bool GBNSender::getWaitingState()
+{
 	return waitingState;
 }
 
-
-
-
-bool StopWaitRdtSender::send(const Message &message) {
-	if (this->waitingState) { //发送方处于等待确认状态
+bool GBNSender::send(const Message &message)
+{
+	if (nextSeqNum >= base + Configuration::WINDOW_SIZE)
+	{
+		this->waitingState = true;
 		return false;
 	}
+	else // 发送窗口未满，可以发送数据报
+	{
+		this->waitingState = false;
+		this->sw[numberOfPacketsInWindow].seqnum = nextSeqNum;
+		this->sw[numberOfPacketsInWindow].acknum = this->nextSeqNum;
+		this->sw[numberOfPacketsInWindow].checksum = 0;
+		memcpy(this->sw[numberOfPacketsInWindow].payload, message.data, sizeof(message.data));
+		this->sw[numberOfPacketsInWindow].checksum = pUtils->calculateCheckSum(this->sw[numberOfPacketsInWindow]);
 
-	this->packetWaitingAck.acknum = -1; //忽略该字段
-	this->packetWaitingAck.seqnum = this->expectSequenceNumberSend;
-	this->packetWaitingAck.checksum = 0;
-	memcpy(this->packetWaitingAck.payload, message.data, sizeof(message.data));
-	this->packetWaitingAck.checksum = pUtils->calculateCheckSum(this->packetWaitingAck);
-	pUtils->printPacket("发送方发送报文", this->packetWaitingAck);
-	pns->startTimer(SENDER, Configuration::TIME_OUT,this->packetWaitingAck.seqnum);			//启动发送方定时器
-	pns->sendToNetworkLayer(RECEIVER, this->packetWaitingAck);								//调用模拟网络环境的sendToNetworkLayer，通过网络层发送到对方
+		pUtils->printPacket("发送方发送报文:", this->sw[numberOfPacketsInWindow]);
+		if (base == nextSeqNum) // 发送窗口中第一个报文，启动定时器
+			pns->startTimer(SENDER, Configuration::TIME_OUT, this->sw[numberOfPacketsInWindow].seqnum);
+		pns->sendToNetworkLayer(SENDER, this->sw[numberOfPacketsInWindow]);
+		this - numberOfPacketsInWindow++;
+		this->nextSeqNum++;
 
-	this->waitingState = true;																					//进入等待状态
-	return true;
+		if (numberOfPacketsInWindow >= Configuration::WINDOW_SIZE)
+			this->waitingState = true;
+
+		return true;
+	}
 }
 
-void StopWaitRdtSender::receive(const Packet &ackPkt) {
-	if (this->waitingState == true) {//如果发送方处于等待ack的状态，作如下处理；否则什么都不做
-		//检查校验和是否正确
-		int checkSum = pUtils->calculateCheckSum(ackPkt);
+void GBNSender::receive(const Packet &ackPkt)
+{
+	if (this->numberOfPacketsInWindow == 0)
+		return;										  // 没有数据报在发送窗口中，直接返回
+	int checkSum = pUtils->calculateCheckSum(ackPkt); // 计算校验和
+	if (checkSum == ackPkt.checksum && ackPkt.acknum >= base)
+	{
+		int num = ackPkt.acknum - base + 1; // 确认号表示已经收到的最大序号
+		base = ackPkt.acknum + 1;
+		this->numberOfPacketsInWindow -= num; // 发送窗口中剩余的报文数
+		pUtils->printPacket("发送方收到确认报文:", ackPkt);
 
-		//如果校验和正确，并且确认序号=发送方已发送并等待确认的数据包序号
-		if (checkSum == ackPkt.checksum && ackPkt.acknum == this->packetWaitingAck.seqnum) {
-			this->expectSequenceNumberSend = 1 - this->expectSequenceNumberSend;			//下一个发送序号在0-1之间切换
-			this->waitingState = false;
-			pUtils->printPacket("发送方正确收到确认", ackPkt);
-			pns->stopTimer(SENDER, this->packetWaitingAck.seqnum);		//关闭定时器
+		if (base = nextSeqNum) // 发送窗口中没有报文，停止定时器
+			pns->stopTimer(SENDER, ackPkt.acknum);
+		else // 发送窗口中还有报文，重启定时器
+		{
+			pns->stopTimer(SENDER, ackPkt.acknum);
+			pns->startTimer(SENDER, Configuration::TIME_OUT, this->sw[0].seqnum);
 		}
-		else {
-			pUtils->printPacket("发送方没有正确收到确认，重发上次发送的报文", this->packetWaitingAck);
-			pns->stopTimer(SENDER, this->packetWaitingAck.seqnum);									//首先关闭定时器
-			pns->startTimer(SENDER, Configuration::TIME_OUT, this->packetWaitingAck.seqnum);			//重新启动发送方定时器
-			pns->sendToNetworkLayer(RECEIVER, this->packetWaitingAck);								//重新发送数据包
 
-		}
-	}	
+		for (int i = 0; i < this->numberOfPacketsInWindow; i++)
+			this->sw[i] = this->sw[i + num]; // 将发送窗口中未确认的报文前移
+	}
 }
 
-void StopWaitRdtSender::timeoutHandler(int seqNum) {
-	//唯一一个定时器,无需考虑seqNum
-	pUtils->printPacket("发送方定时器时间到，重发上次发送的报文", this->packetWaitingAck);
-	pns->stopTimer(SENDER,seqNum);										//首先关闭定时器
-	pns->startTimer(SENDER, Configuration::TIME_OUT,seqNum);			//重新启动发送方定时器
-	pns->sendToNetworkLayer(RECEIVER, this->packetWaitingAck);			//重新发送数据包
-
+void GBNSender::timeoutHandler(int seqNum)
+{
+	pUtils->printPacket("发送方定时器时间到，重发窗口中所有报文:", this->sw[0]);
+	pns->stopTimer(SENDER, this->sw[0].seqnum);
+	pns->startTimer(SENDER, Configuration::TIME_OUT, this->sw[0].seqnum);
+	for (int i = 0; i < this->numberOfPacketsInWindow; i++)
+	{
+		pUtils->printPacket("发送方重发报文:", this->sw[i]);
+		pns->sendToNetworkLayer(RECEIVER, this->sw[i]);
+	}
 }
